2925564ab4a36927510af95909598d0f0b56510e
fabio cherici <fabiocherici@gmail.com>
2025-10-10 14:58:39 +0200

    [FIX] Correct SVG heart icon path in likes widgets
    **PROBLEMA:**
    Errore SVG: Expected arc flag ('0' or '1') in path attribute
    - Path errato: a4 4 0 515.656 (mancavano spazi e flag)
    - Browser non poteva renderizzare l'icona cuore
    
    **CORREZIONE:**
    - Path corretto: a4 4 0 0 1 5.656 (arc con flag corretti)
    - Applicato a 2 file:
      * egi-likes-received-widget.blade.php
      * like-received-analytics-widget.blade.php
    
    **SPIEGAZIONE PATH SVG:**
    a4 4 0 0 1 5.656 = arc(rx ry x-axis-rotation large-arc-flag sweep-flag x y)
    - rx, ry: 4, 4 (raggi ellisse)
    - x-axis-rotation: 0 (nessuna rotazione)
    - large-arc-flag: 0 (arco piccolo)
    - sweep-flag: 1 (senso orario)
    - endpoint: 5.656 (coordinate finali)
    
    Icona cuore ora renderizza correttamente in tutti i browser.

--- resources/views/components/stats/like-received-analytics-widget.blade.php
+++ resources/views/components/stats/like-received-analytics-widget.blade.php
@@ -195,7 +195,7 @@
                             <div class="flex-shrink-0 text-right">
                                 <div class="flex items-center text-pink-400">
                                     <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 515.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path>
+                                        <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 0 1 5.656 0L10 6.343l1.172-1.171a4 4 0 1 1 5.656 5.656L10 17.657l-6.828-6.829a4 4 0 0 1 0-5.656z" clip-rule="evenodd"></path>
                                     </svg>
                                     <span class="text-sm font-bold">{{ $user['likes_count'] }}</span>
                                 </div>
