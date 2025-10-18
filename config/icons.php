044c06c135fd7b0c8e480ef88abe9d5f39693a6e
fabio cherici <fabiocherici@gmail.com>
2025-10-18 12:22:22 +0200

    [FIX] Aggiunte Material Icons per menu PA (smart_toy, bar_chart, auto_mode)

--- config/icons.php
+++ config/icons.php
@@ -117,6 +117,31 @@ return [
                     <path fill-rule="evenodd" d="M3.75 6.75a3 3 0 00-3 3v6a3 3 0 003 3h15a3 3 0 003-3v-.037c.856-.174 1.5-.93 1.5-1.838v-2.25c0-.907-.644-1.664-1.5-1.837V9.75a3 3 0 00-3-3h-15zm15 1.5a1.5 1.5 0 011.5 1.5v6a1.5 1.5 0 01-1.5 1.5h-15a1.5 1.5 0 01-1.5-1.5v-6a1.5 1.5 0 011.5-1.5h15zM4.5 9.75a.75.75 0 00-.75.75V15c0 .414.336.75.75.75H18a.75.75 0 00.75-.75v-4.5a.75.75 0 00-.75-.75H4.5z" clip-rule="evenodd" />
                 </svg>',
             ],
+            // Material Icons per N.A.T.A.N. PA
+            [
+                'name' => 'smart_toy',
+                'type' => 'material',
+                'class' => 'w-5 h-5',
+                'host' => 'https://fonts.google.com/icons',
+                'name_on_host' => 'smart_toy',
+                'html' => '<span class="material-symbols-outlined" style="font-size:20px;color:#D4A574;">smart_toy</span>',
+            ],
+            [
+                'name' => 'bar_chart',
+                'type' => 'material',
+                'class' => 'w-5 h-5',
+                'host' => 'https://fonts.google.com/icons',
+                'name_on_host' => 'bar_chart',
+                'html' => '<span class="material-symbols-outlined" style="font-size:20px;color:#D4A574;">bar_chart</span>',
+            ],
+            [
+                'name' => 'auto_mode',
+                'type' => 'material',
+                'class' => 'w-5 h-5',
+                'host' => 'https://fonts.google.com/icons',
+                'name_on_host' => 'auto_mode',
+                'html' => '<span class="material-symbols-outlined" style="font-size:20px;color:#D4A574;">auto_mode</span>',
+            ],
             'user-cog' => [
                 'name' => 'Gestione Utente',
                 'type' => 'heroicon',
