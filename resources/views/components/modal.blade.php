f04076157a16e0b8c5e02b0c0a0c707ce8691575
fabio cherici <fabiocherici@gmail.com>
2026-02-02 23:07:03 +0100

    [FIX] Modal Component Critical Patch
    - Wrapped @entangle directive in @if check for wire:model
    - Prevents crash in non-Livewire contexts (like standard Blade views)
    - Finalizes compatibility for Commerce notifications

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -14,7 +14,7 @@
     ][$maxWidth ?? '2xl'];
 @endphp
 
-<div x-data="{ show: @entangle($attributes->wire('model')) }" x-on:close.stop="show = false" x-on:keydown.escape.window="show = false" x-show="show"
+<div x-data="{ show: @if ($attributes->whereStartsWith('wire:model')->first()) @entangle($attributes->wire('model')) @else false @endif }" x-on:close.stop="show = false" x-on:keydown.escape.window="show = false" x-show="show"
     id="{{ $id }}" class="jetstream-modal fixed inset-0 z-50 overflow-y-auto px-4 py-6 sm:px-0"
     style="display: none;">
     <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false"
